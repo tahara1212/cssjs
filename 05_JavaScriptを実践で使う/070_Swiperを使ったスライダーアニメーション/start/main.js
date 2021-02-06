6707f80a4e19011f8bf9a7d7e9a9d0e8590fb514
田原隼併 <t.pe1shun@gmail.com>
2021-02-06 19:18:02 +0900

    20210206

--- 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -1,6 +1,38 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    // direction: 'vertical',
-    effect: 'coverflow',
-    loop: true,
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  const hero = new HeroSlider('.swiper-container');
+});
+
+class HeroSlider {
+  constructor(el) {
+    this.el = el;
+    this.swiper = this._initSwiper();
+  }
+
+  _initSwiper() {
+    return new Swiper(this.el, {
+      // Optional parameters
+      // direction: 'vertical',
+      effect: 'coverflow',
+      loop: true,
+      // スライドを中央配置。
+      centeredSliders: true,
+      // 表示するスライドの数。
+      slidesPerView: 1,
+      // スライドからスライドへのduration。
+      speed: 1000,
+      grabCursor: true,
+      // 画面幅に応じてスライドの表示枚数を制御。
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+        }
+      },
+      // 自動スライド設定。
+      autoplay: {
+        delay: 1000,
+        // マウスでスライドさせた後も自動スライドさせる。
+        disableOnInteraction: false
+      }
+    });
+  }
+}
